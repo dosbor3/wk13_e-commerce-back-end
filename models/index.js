d1a4b94f4091e0bc211ae53fe74dc2be6ec5e975
I am Billie <dosbor3@gmail.com>
2022-02-27 10:56:38 -0600

    Models Implemented

--- models/index.js
+++ models/index.js
@@ -5,8 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Catagory.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
