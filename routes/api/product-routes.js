b48ad6c5d3c963e4e52cc82b5b351d090736b61f
I am Billie <dosbor3@gmail.com>
2022-02-27 14:01:14 -0600

    product-route API complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,40 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll()
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        include: {
+          model: ProductTag,
+          attributes: ['id', 'product_id', 'tag_id']
+        }
+      }
+    ]
+  })    
+    
 });
 
 // create new product
